77646913c1b9c766ef96b73eaafdd5a48f8a684f
swizzle1999 <key.key@hotmail.co.uk>
2026-01-09 17:19:16 +0000

    Add project files.

---
+++ Services/UIEventService.cs
@@ -0,0 +1,6 @@
+﻿namespace WebsiteWASM.Services {
+    public class UIEventService {
+        public event EventHandler? TerminalCloseRequested;
+        public void RequestTerminalClose(object? sender, EventArgs e) => TerminalCloseRequested?.Invoke(sender, e);
+    }
+}
