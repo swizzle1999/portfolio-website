d29c225e3aaa51c514ebb7a28db3fb0738dc1da6
swizzle1999 <key.key@hotmail.co.uk>
2026-01-11 14:07:18 +0000

    Refactored the "window" into its own, re-usable class

--- Services/UIEventService.cs
+++ Services/UIEventService.cs
@@ -1,6 +1,9 @@
 ﻿namespace WebsiteWASM.Services {
     public class UIEventService {
         public event EventHandler? TerminalCloseRequested;
+        public event EventHandler? FileBrowserCloseRequested;
+
         public void RequestTerminalClose(object? sender, EventArgs e) => TerminalCloseRequested?.Invoke(sender, e);
+        public void RequestFileBrowserClose(object? sender, EventArgs e) => FileBrowserCloseRequested?.Invoke(sender, e);
     }
 }
