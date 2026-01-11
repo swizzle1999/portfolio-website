f65a129053904f81f50e195d19c0768969da5586
swizzle1999 <key.key@hotmail.co.uk>
2026-01-11 15:54:39 +0000

    Re-factored the window display logic slightly. Windows now have a "Window" component which handles the title bar and visibility

---
+++ Pages/Desktop/Windows/IWindow.cs
@@ -0,0 +1,8 @@
+﻿namespace WebsiteWASM.Pages.Desktop.Windows {
+    public interface IWindow {
+        public void Show();
+        public void Hide();
+        public void Toggle();
+        public IWindow? Window { get; }
+    }
+}
